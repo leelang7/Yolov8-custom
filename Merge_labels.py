d8e1ff0c05792499af116c8fb3e73e8e53bd131e
Fixer <67309088+leelang7@users.noreply.github.com>
2023-11-17 15:41:47 +0900

    Update Merge_labels.py

--- Merge_labels.py
+++ Merge_labels.py
@@ -1,7 +1,7 @@
 import os
 
-YOLOV8_LABEL_ROOT = f"E:\AI_Project\Smart_Construction_Project\inference\output\\"  # yolov8이 내보낸 추론 이미지의 txt
-DATASET_LABEL_ROOT = f"E:\AI_Project\AI_Learning\Dataset\VOC2028\Labels\\"  # 사람이 없는 원본 데이터셋의 라벨 경로
+YOLOV8_LABEL_ROOT = "C:\\Users\\leesc\\PycharmProjects\\ultralytics\\results\\"  # 사람 클래스가 들어있는 txt
+DATASET_LABEL_ROOT = "C:\\Users\\leesc\\PycharmProjects\\ultralytics\\runs\\detect\\predict3\\labels\\"  # 사람이 없는 커스텀 모델이 내보낸 원본 train set 추론 결과 라벨 경로(백업 추천)
 
 yolo_file = os.listdir(YOLOV8_LABEL_ROOT)
 
@@ -16,13 +16,14 @@ for file_name in yolo_file:
     with open(file_path, "r") as f:
         for line in f.readlines():
 
-            # 0 -> person 데이터만 추출
-            if line.split()[0] != '0':
+            # class가 person인 라인만 추출
+            if line.split()[0] != '2':
                 continue
 
             data_path = DATASET_LABEL_ROOT + file_name
             print(data_path)
             # 주석파일에 추가
             with open(data_path, "a") as fd:
+                cnt += 1
                 fd.write(line)
 print(f'{cnt} 라인 추가 완료')
